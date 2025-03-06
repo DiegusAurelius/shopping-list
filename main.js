33266e88b6400dca932ff438a0e822c9c863955e
DiegusAurelius <diegusaurelius@gmail.com>
2025-03-05 20:25:47 -0600

    Add shopping-list

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('It works!');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+const addNewItem = () => {
+  const newItem = input.value.trim();
+  if (!newItem) return;
+
+  input.value = '';
+
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const btnDelete = document.createElement('button');
+
+  span.textContent = newItem;
+  btnDelete.textContent = 'Delete';
+
+  li.appendChild(span);
+  li.appendChild(btnDelete);
+  ul.appendChild(li);
+
+  btnDelete.addEventListener('click', () => ul.removeChild(li));
+
+  input.focus();
+};
+
+button.addEventListener('click', addNewItem);
+
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addNewItem();
+  }
+});
